2fca6abc7d02309019c2fb40c7434e849bb38ab9
Jaume Sánchez <jasato89@gmail.com>
2022-09-22 14:53:02 +0200

    BigDecimal

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,30 @@ import classes.ApplePlayer;
 import classes.SavingsAccount;
 import interfaces.Playable;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        SavingsAccount account = new SavingsAccount("Jaume", 3500000, 1.01);
 
+
+        double a = .7;
+        double b = .1;
+        double c = .2;
+
+        System.out.println(a + b);
+
+        BigDecimal a1 = new BigDecimal("1").setScale(2);
+        BigDecimal b1 = new BigDecimal(".2").setScale(8);
+
+        BigDecimal result1 = a1.divide(b1);
+
+        System.out.println(result1);
+
+        System.out.println(result1.pow(2));
+
+        System.out.println(Math.pow(result1.doubleValue(), 2));;
     }
 }
